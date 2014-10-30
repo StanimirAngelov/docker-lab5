6b8039f871a4143b49bba30d10eeaf9764965f05
Stanimir Angelov <c12733849@mydit.ie>
2014-10-30 18:30:47 +0200

    updating app try one

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+import os, sys
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -14,3 +16,12 @@ def upload_file():
     return '',201
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+@app.route("/listfiles")
+def files():
+    path = "./uploads"
+    dirs = os.listdir(path)
+
+    for file in dirs:
+	files = str(files + file + " ")
+    return files 
